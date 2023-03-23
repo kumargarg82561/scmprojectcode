bf40736885d4c928489a4fad7cb7c3d368f5274c
kumargarg <kgargkgarg9@gmail.com>
2023-03-23 20:59:00 +0530

    first commit

---
+++ rpsg.py
@@ -0,0 +1,19 @@
+import random
+
+while True:
+    playeraction = input("Enter a choice (rock, paper, scissors): ")
+    actionpossible = ["rock", "paper", "scissors"]
+    laptopaction = random.choice(actionpossible)
+    print(f"\nYou chose {playeraction}, computer chose {laptopaction}.\n")
+
+    if playeraction == laptopaction:
+        print(f"Both players selected {playeraction}. It's a tie!")
+    elif playeraction == "rock":
+        if laptopaction == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif playeraction == "paper":
+        if laptopaction == "rock":
+            print("Paper covers rock! You win!")
+
